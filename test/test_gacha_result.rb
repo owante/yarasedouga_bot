f3ab7248c2ee8bb28d1fd0622523c3c12b07cded
owante <owante.ojisan@gmail.com>
2018-05-10 16:17:26 +0900

    Fix test codes

--- test/test_gacha_result.rb
+++ test/test_gacha_result.rb
@@ -1,12 +1,16 @@
 require 'main'
 
+module TestProbability
+  PROBABILITY = Gacha::MOST_RARE_PROBABILITY
+end
+
 class GachaFeatureTest < Test::Unit::TestCase
   def test_gacha_result_by_set
     puts '100セット10回で成功する回数を検証します：'
     10.times do |turn|
       count = 0
       100.times do
-        result = Gacha.gacha
+        result = Gacha.gacha(TestProbability::PROBABILITY)
         count += 1 if result == Gacha::Result::WON
       end
       puts "ガチャ100回（#{turn + 1}セット目）のヤラセ成功回数：#{count}回"
@@ -15,16 +19,18 @@ class GachaFeatureTest < Test::Unit::TestCase
   end
 
   def test_gacha_result_by_day
-    day = 12
+    day = 8
     total_days = 30
     puts "1日#{day}回を1ヶ月引いて成功する回数を検証します："
     total_count = 0
     total_days.times do |turn|
       count = 0
       day.times do
-        result = Gacha.gacha
-        count += 1 if result == Gacha::Result::WON
-        total_count += 1 if result == Gacha::Result::WON
+        result = Gacha.gacha(TestProbability::PROBABILITY)
+        if result == Gacha::Result::WON
+          count += 1
+          total_count += 1
+        end
       end
       puts "#{turn + 1}日目のヤラセ成功回数：#{count}回"
     end
